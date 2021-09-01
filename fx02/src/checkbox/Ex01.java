c9613ea113c98035907b303a841eecc616854475
parkseungjae <tmd0915mp@naver.com>
2021-09-01 09:18:14 +0900

    components 시작

--- fx02/src/checkbox/Ex01.java
+++ fx02/src/checkbox/Ex01.java
@@ -1,16 +1,17 @@
-package quiz;
+package checkbox;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Quiz01 extends Application {
+public class Ex01 extends Application{
 
 	@Override
 	public void start(Stage arg0) throws Exception {
 		arg0.show();
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
