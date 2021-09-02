e78eabf11eade6f473c0a0554cd4c76d8e102619
parkseungjae <tmd0915mp@naver.com>
2021-09-02 14:32:16 +0900

    activity 만들기 (controller.class)

--- fx03_scene/src/ex02/Controller.java
+++ fx03_scene/src/ex02/Controller.java
@@ -1,5 +1,25 @@
 package ex02;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller implements Initializable{
+	@FXML private Button btn;//fxml파일의 id와 동일하게 입력해주어야한다.
+	@FXML private TextField txtField;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		System.out.println("초기화 목적으로 사용합니다.");
+		btn.setOnAction(e->{
+			txtPrint();
+		});
+	}
+	public void txtPrint() {
+		System.out.println(txtField.getText());
+	}
 }
--- fx03_scene/src/ex02/MainClass.java
+++ fx03_scene/src/ex02/MainClass.java
@@ -6,14 +6,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class MainClass extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws IOException{
+		System.out.println("11111");
 		Parent root = FXMLLoader.load(getClass().getResource("eventTest.fxml"));
+		System.out.println("22222");
 		Scene scene = new Scene(root);
+		System.out.println("33333");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
