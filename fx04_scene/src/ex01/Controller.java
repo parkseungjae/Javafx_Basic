10f3838e0579dc161d9812c99b65cdcd06047e3c
parkseungjae <tmd0915mp@naver.com>
2021-09-03 11:00:42 +0900

    fx04 진행

--- fx04_scene/src/ex01/Controller.java
+++ fx04_scene/src/ex01/Controller.java
@@ -1,5 +1,15 @@
 package ex01;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class Controller implements Initializable{
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
 
 }
